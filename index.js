10d1dc61476eabd9c6d42f701f4fe486bed41ddf
fantasyui-com <captain@fantasyui.com>
2018-07-12 18:15:58 -0400

    Add code comments, and cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var kebabCase = require('lodash/kebabcase');
 
 module.exports = function(options){
-
   const isAttributes = function(o){
     if(typeof o === 'object'){
       if(o.nodeType) {
@@ -10,7 +9,6 @@ module.exports = function(options){
         return true;
       }
     }
-
   }
   const isElements = function(o){
     if(o.nodeType || typeof o === 'string') {
@@ -28,10 +26,13 @@ module.exports = function(options){
   }
 
   const c = function(nodeName='div', ...data){
-
     const attributes = new Map();
     const classes = new Set();
     const elements = [];
+
+    //
+    // REGEXP SECTION
+    //
 
     // Extract String Based Attributes (shorthands)
     function idReplacer(match, capture, offset, string) {
@@ -39,49 +40,60 @@ module.exports = function(options){
       return '';
     }
     var nodeName = nodeName.replace(/\#([a-z0-9_-]+)/i, idReplacer);
-
     function classReplacer(match, capture, offset, string) {
       classes.add(capture)
       return '';
     }
     var nodeName = nodeName.replace(/\.([a-z0-9_-]+)/ig, classReplacer);
 
+    //
+    // ATTRIBUTES
+    //
+
+    Note in some cases attribute value is yet another object (styles)
+
     // Extract Object Based Attributes
     data.filter(o=>isAttributes(o)).forEach(function(objectAttributes){
-
       Object.keys(objectAttributes).forEach(function(attribute){
 
         // if value is an object then this is attributes.style = ...
         if(typeof objectAttributes[attribute] === 'object'){
-          // it is an attribute object
+          // it is an attribute with an object based value style = {color: 'red'}
           const attributeName = attribute;
-          let attributeValue = '';
+          let attributeValue = ''; // prepare the string to be built
+          // Build the string....
           Object.keys(objectAttributes[attribute]).forEach(function(subAttribute){
             const key = subAttribute;
             const val = objectAttributes[attribute][subAttribute];
             attributeValue = attributeValue + `${kebabCase(key)}: ${val}; `
           })
+          // clean it
           attributeValue = attributeValue.trim();
+          // store it for latr use
           attributes.set(attributeName, attributeValue)
 
-        // if value is a string
         }else if(attribute === 'class'){
 
+          // Attribute is a class, we use a Set here as multiple sources of class are present, set will dedupe
           const attributeName = attribute;
           const attributeValue = objectAttributes[attribute];
+          // store it for latr use
           attributeValue.split(" ").forEach(function(className){
             classes.add(className)
-          })
+          });
 
         } else {
+
+          // not a value object (styles)
+          // not a class (dedupe)
+          // just a plain text property
           const attributeName = attribute;
           const attributeValue = objectAttributes[attribute];
+          // store it for latr use
           attributes.set(attributeName, attributeValue)
+
         }
-
-
       })
-
     })
 
     // Extract Child Nodes
@@ -93,20 +105,24 @@ module.exports = function(options){
       }
     })
 
-    //DONE
+    //
+    // DATA PREPARATIONS COMPLETE, apply readied data to element.
+    //
+
+    // Finally, create the new element.
     const element = document.createElement(nodeName);
 
-
+    // attributes, all values here are strings prepped up above
     for (var [attributeName, attributeValue] of attributes) {
       element.setAttribute(attributeName, attributeValue);
     }
 
-    classes.forEach(function(e){
-      const list = (element.getAttribute('class')||'').split(" ").filter(i=>i.trim());
-      list.push(e);
-      element.setAttribute('class', list.join(" "))
-    })
+    // Take care of class="" ensure no dupes, etc...
+    // note this behaves as if element already had some classes, future proof...
+    (element.getAttribute('class')||'').split(" ").filter(i=>i.trim()).forEach(function(class){classes.add(class)})
+    element.setAttribute('class', Array.from(classes).join(" "))
 
+    // add children, these already ran...
     elements.forEach(function(e){
       element.appendChild(e)
     })
--- test.js
+++ test.js
@@ -23,23 +23,20 @@ function enc(s) {
 	return s.replace(/[&'"<>]/g, function(a){ return `&#${a};` });
 }
 
-// document.body.appendChild(
-//   c( 'p.bbb', {id:'foo', class:"ggg aaa", style:{float: 'left', width: '200px', backgroundColor: 'red'}},
-//     'XXX',
-//     c('x#moo.foo.ggg')
-//   )
-// )
+
 
 
 document.body.appendChild(
   c('div#page',
     c('div#header',
       c('h1.classy', 'h')),
+
     c('div#menu', { style: { float: 'left', width: '200px' } },
       c('ul',
         c('li', 'one'),
         c('li', 'two'),
         c('li', 'three'))),
+
     c('div#content', {style: {float: 'left'} },
       c('h2', 'content title'),
       c('p',
