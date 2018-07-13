32552347727765d73e7b4b7e042d48ee49dc38c1
fantasyui-com <captain@fantasyui.com>
2018-07-13 16:45:43 -0400

    Dynamic Data Binding Examples

--- test.js
+++ test.js
@@ -32,18 +32,17 @@ document.body.appendChild(
 
 
       // BEGIN BOUND COMPONENT USAGE EXAMPLE (named content areas)
-      binder( '/hello/abc', (envelope)=>
-  			cBootstrapCard( { class:"bork", style: { float: 'left', width: '200px' }}, {
-  				header: c('div.card-title', 'Mios Dios!'),
+      binder( '/hello/abc', (envelope)=>cBootstrapCard(
+          { class:"bork", style: { float: 'left', width: '200px' }}, {
+  				header: c('div.card-title', envelope.data.value),
   				body: c('ul',
   					c('li', 'one'),
   	        c('li', 'two'),
   	        c('li', 'three'),
-  					['four','five','six'].map(i=>c('li',i)),
+  					envelope.data.values.map(i=>c('li',i)),
   				),
   				footer: c('div.card-text', 'Status: Nominal!')
-  			})
-      ),
+  			})),
   			// END BOUND COMPONENT USAGE EXAMPLE
 
       // c('ul#data11.code-red',
@@ -92,9 +91,9 @@ let counter = 1;
 const fun = function(){
   counter++
 	configuration.emitter.emit('/hello/abc', [
-		{meta:{ identity:'idc219000b8e8f4ea7a125ae1023cc821d', version:'3453,5756a44f3b0141d3aa5627f506181c2f',    token:'b5115bc0961247eaa6eb1dba54b204ed', deleted:false}, data:{value:'a'+counter}},
-		{meta:{ identity:'id8849201d21cb44dda43f012d89a2a101', version:'3458535,efc6d0f6f9d243539efc6c8cf63120c2', token:'699a268232dc4f139cbc3c34906dcd79', deleted:true}, data:{value:'b'+counter}},
-		{meta:{ identity:'ide4c6dc676f744fdbbd8560f0768cdb5e', version:'6458736,ef8442e28a0a473bb90a586b70557513', token:'c7df13597f4a4479befeb211fb857beb', deleted:false}, data:{value:'c'+counter}},
+		{meta:{ identity:'idc219000b8e8f4ea7a125ae1023cc821d', version:'3453,5756a44f3b0141d3aa5627f506181c2f',    token:'b5115bc0961247eaa6eb1dba54b204ed', deleted:false}, data:{value:'a'+counter, values:['four','five','six','x'+counter]}},
+		{meta:{ identity:'id8849201d21cb44dda43f012d89a2a101', version:'3458535,efc6d0f6f9d243539efc6c8cf63120c2', token:'699a268232dc4f139cbc3c34906dcd79', deleted:true}, data:{value:'b'+counter, values:['four','five','six','x'+counter]}},
+		{meta:{ identity:'ide4c6dc676f744fdbbd8560f0768cdb5e', version:'6458736,ef8442e28a0a473bb90a586b70557513', token:'c7df13597f4a4479befeb211fb857beb', deleted:false}, data:{value:'c'+counter, values:['four','five','six','x'+counter]}},
 	])
 
 };
